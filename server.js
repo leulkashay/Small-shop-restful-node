48f8dfb7de23554cba4aed1775adff844e36c58a
amh21-Ha <amha.ha07@gmail.com>
2023-07-30 21:50:38 +0300

    well done

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const http = require("http");
+const app = require('./app');
+//const host = 'localhost';
+const port = process.env.PORT || 3000;
 
-const host = 'localhost';
-const port = 8000;
-
-const requestListener = function(req, res) {
+/*const requestListener = function(req, res) {
 	    res.writeHead(200);
 	    res.end("Hello World from Node.js HTTP Server");
-}
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-	    console.log(`Server is running on http://${host}:${port}`);
-});
+}*/
+const server = http.createServer(app);
+server.listen(port);
