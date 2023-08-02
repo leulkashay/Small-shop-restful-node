c2285a4476fec716be7b0021d5a477557c61db6a
amh21-Ha <amha.ha07@gmail.com>
2023-08-02 13:49:10 +0300

    more routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,16 +1,38 @@
-const exports = require('express');
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        massage: 'Handiling GET requests to /products'
+        message: 'Handiling GET requests to /products'
     });
 });
 
-router.post('/',(req, res, next) => {
-    res.status(200).json({
-        massage: 'Handiling POST requests to /products'
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handiling POST requests to /products'
     });
 });
-
+router.get('/:productId', (req, res, next) => {
+    const id =req.params.productsId;
+    if (id==='special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+    });
+    router.patch('/:productId', (req, res, next) => {
+        res.status(200).json({
+            message: 'Updated product!'
+        });
+    })
+    router.delete('/:productId', (req, res, next) => {
+        res.status(200).json({
+            message: 'Deleted product!'
+        });
+    })
 module.exports = router;
